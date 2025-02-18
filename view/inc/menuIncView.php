7f3648012568188c2c52b8ba66c1937866650356
Mykyta Horban <masarash12345@gmail.com>
2025-02-19 00:09:01 +0100

    buttons new style

--- view/inc/menuIncView.php
+++ view/inc/menuIncView.php
@@ -4,16 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-
     <title>Site</title>
 </head>
 <body>
     <nav class="navbar">
         <ul class="navbar-menu">
-            <li class="navbar-item"><a href="?page=accueil" class="navbar-link">Accueil</a></li>
-            <li class="navbar-item"><a href="?page=apropos" class="navbar-link">Apropos</a></li>
-            <li class="navbar-item"><a href="?page=contact" class="navbar-link">Contact</a></li>
-            <li class="navbar-item"><a href="?page=histoire" class="navbar-link">Histoire</a></li>
+            <li class="navbar-item"><a href="?page=accueil" class="navbar-link button">Accueil</a></li>
+            <li class="navbar-item"><a href="?page=apropos" class="navbar-link button">Apropos</a></li>
+            <li class="navbar-item"><a href="?page=contact" class="navbar-link button">Contact</a></li>
+            <li class="navbar-item"><a href="?page=histoire" class="navbar-link button">Histoire</a></li>
         </ul>
     </nav>
 </body>
